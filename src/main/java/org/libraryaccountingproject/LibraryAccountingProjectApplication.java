9e1b29f131752c544e437e4701b4f6cdc45ab800
SergeiVS <fam.smirnoff@gmail.com>
2024-08-18 19:29:37 +0200

    initial commit

---
+++ src/main/java/org/libraryaccountingproject/LibraryAccountingProjectApplication.java
@@ -0,0 +1,13 @@
+package org.libraryaccountingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAccountingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAccountingProjectApplication.class, args);
+    }
+
+}
